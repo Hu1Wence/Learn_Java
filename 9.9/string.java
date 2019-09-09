698e919b630c4968e400d9fd4e435dd73ca9db86
xiaohutongxue <929125066@qq.com>
2019-09-09 22:27:49 +0800

    Create string.java

---
+++ 9.9/string.java
@@ -0,0 +1,9 @@
+public class string {
+    public static void main(String[] args) {
+        String str = "result = ";
+        int a = 10;
+        int b = 20;
+        String result = str + a + b;
+        System.out.println(result);
+    }
+}
