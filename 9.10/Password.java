c1f49cae6417d29b23b5724394c22a1d21274536
xiaohutongxue <929125066@qq.com>
2019-09-10 23:01:44 +0800

    Create Password.java

---
+++ 9.10/Password.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Password {
+    public static void main(String[] args) {
+        String password = "hu123456";
+        Scanner my_password = new Scanner(System.in);
+        
+
+    }
+}
