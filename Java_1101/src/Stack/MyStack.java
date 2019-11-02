68f824382d484ee0372da18753ccbfacdc8d1e34
xiaohutongxue <929125066@qq.com>
2019-11-02 17:23:49 +0800

    Java_1101

--- Java_1101/src/Stack/MyStack.java
+++ Java_1101/src/Stack/MyStack.java
@@ -1,40 +1,41 @@
 package Stack;
 
 public class MyStack {
-    private int[] array = new int[100];
-    private int size = 0;
+    public class MyStack {
+        private int[] array = new int[100];
+        private int size = 0;
 
-    //压栈
-    public void push(int val) {
-        array[size] = val;
-        size++;
-    }
-
-    //出栈
-    public Integer pop() {
-        if (size == 0) {
-            return null;
+        //压栈
+        public void push(int val) {
+            array[size] = val;
+            size++;
         }
-        int ret = array[size - 1];
-        size--;
-        return ret;
-    }
 
-    //查看栈顶元素
-    public Integer peek() {
-        if (size == 0) {
-            return null;
+        //出栈
+        public Integer pop() {
+            if (size == 0) {
+                return null;
+            }
+            int ret = array[size - 1];
+            size--;
+            return ret;
         }
-        return array[size - 1];
-    }
 
-    //判断栈是否为空
-    public boolean isEmpty() {
-        return size == 0;
-    }
+        //查看栈顶元素
+        public Integer peek() {
+            if (size == 0) {
+                return null;
+            }
+            return array[size - 1];
+        }
 
-    //查看栈的大小
-    public int size() {
-        return size;
-    }
+        //判断栈是否为空
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        //查看栈的大小
+        public int size() {
+            return size;
+        }
 }
